330cd7bffc2fd84226db4fcc303f0f20f98181df
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-03-06 13:40:13 +0000

    json data linked

--- Frontend/src/app/musicServiceandData/music.ts
+++ Frontend/src/app/musicServiceandData/music.ts
@@ -1,2 +1,15 @@
 export interface Music {
+    id: number,
+    trackName: string,
+    trackLink: string,
+    artist: string,
+    album: string,
+    recordedDate: Date,
+    coverArt: string,
+    sourcedFrom: string,   
+    genre: string,
+    availableForSale: boolean,  
+    price: number,
+    comment: string
+     
 }
--- Frontend/src/app/postcard/postcard.component.ts
+++ Frontend/src/app/postcard/postcard.component.ts
@@ -28,12 +28,27 @@ export class PostcardComponent implements OnInit {
   music: Music | undefined;
   comments: FormGroup;
 
+  
+
+  //FormBuilder service is used to create a form group. The form group is used to create a form control for the comment field.
+  //Comment field is required to validate for submission. 
+  //Private = only accessible within the class.So stops unintended interfernce by other code external to the class.
   constructor(private formBuilder: FormBuilder) {
     this.comments = this.formBuilder.group({
       comment: ['', Validators.required],
     });
+    
+
   }
 
+  //Method to get music by ID. Uses the musicService to get the music by ID.
+  //
+  
+  //NgOnInit is a lifecycle hook that is called after Angular has initialized all data-bound properties of a directive.
+  // The ngOnInit() method is called when the component is initialized.
+  //The route parameter is used to get the music ID from the URL.
+  // This NgOnInit method is used to get the music by ID using the musicService.
+  //This method ensures that the initialization logic runs at the appropriate time in the component's lifecycle
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const musicId = Number(params.get('id'));
@@ -49,8 +64,14 @@ export class PostcardComponent implements OnInit {
     });
   }
 
+  //Method to submit comment. This method is called when the user submits a comment.
+  //?? = nullish coalescing operator. It returns the right-hand operand when the left-hand operand is null or undefined.
+  //If the comment is valid, the submitComment() method calls the musicService.submitComment() method to submit the comment.
+  //The comment is retrieved from the form control using the value property.
+  //If there is no comment, the comment is set to an empty string.
   submitComment(): void {
-    if (!this.comments.valid) {
+    if (this.comments.valid) {
+      //do we want to be able to stop comment submission if there is no comment?
       this.musicService.submitComment(this.comments.value.comment ?? '');
     }
   }
