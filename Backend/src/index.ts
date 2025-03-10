304282b4b2128c9d8ea48194937c95486b6006ca
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-03-10 11:55:56 +0000

    database connection acheived

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+const app: Express = express();
+const mongoose: any = require('mongoose');
+const port = process.env.PORT || 3001;
+
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT || 3001;
+async function main() {
+  const connection = await mongoose.connect(process.env.MONGODB_URL);
+  console.log(`Successfully connected to MongoDB!\n${connection.connection.host}`);
+}
+
+main().catch(error => console.error(error));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
