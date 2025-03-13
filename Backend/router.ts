ac3e34bcdceec7148bff82bed630c5701fda9bdb
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-03-13 16:28:27 +0000

    getMusicbyID endpoint added

--- Backend/router.ts
+++ Backend/router.ts
@@ -2,10 +2,11 @@ import express from "express";
 const router = express.Router();
 
 // Import the controllers
-import { createMusicPost, getMusicPosts } from './musicController';
+import { createMusicPost, getMusicPosts, getMusicPostByID } from './musicController';
 
 // Define the routes
 router.post('/music', createMusicPost); // Create a new music post
 router.get('/music', getMusicPosts); // Get all music posts
+router.get('/music/:id', getMusicPostByID); // Get a single music post
 
 export default router;
