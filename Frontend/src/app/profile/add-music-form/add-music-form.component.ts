dbf4062a52b9403587e18f55e113191dc6dd4fde
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-03-21 13:14:14 +0000

    form inputs changed to material components. Scrool on detail component changed

--- Frontend/src/app/profile/add-music-form/add-music-form.component.ts
+++ Frontend/src/app/profile/add-music-form/add-music-form.component.ts
@@ -2,34 +2,29 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MusicService } from '../../musicServiceandData/music.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
+  
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
 
 @Component({
   selector: 'app-add-music-form',
-  imports: [CommonModule, RouterModule, ReactiveFormsModule],
+  standalone: true,
+  imports: [CommonModule, RouterModule, ReactiveFormsModule, MatInputModule, MatButtonModule, MatFormFieldModule, MatSelectModule],
   templateUrl: './add-music-form.component.html',
-  styleUrls: ['./add-music-form.component.css']
+  styleUrls: ['../../../styles.scss'],
 })
 export class AddMusicFormComponent {
   musicService: MusicService = inject(MusicService);
-  MusicPostForm: FormGroup = new FormGroup({
-    trackName: new FormControl('', Validators.required),
-    trackLink: new FormControl('', Validators.required),
-    artist: new FormControl('', Validators.required),
-    album: new FormControl('',),
-    recordedDate: new FormControl('', ),
-    coverArt: new FormControl('', Validators.required),
-    sourcedFrom: new FormControl('', Validators.required),
-    genre: new FormControl('', Validators.required),
-    availableForSale: new FormControl('', Validators.required), 
-    price: new FormControl('',)
-  });
+  MusicPostForm: FormGroup;
 
   constructor(private formBuilder: FormBuilder) {
     this.MusicPostForm = this.formBuilder.group({
@@ -51,6 +46,4 @@ export class AddMusicFormComponent {
     console.log('Music data:', musicData);
     // await this.musicService.addMusic(musicData);
   }
-
-
 };
